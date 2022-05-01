25dab857f1243867fe71566ca0d03a00af54a542
Regolith247 <77996630+Regolith247@users.noreply.github.com>
2022-04-30 22:56:11 -0600

    revision again

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayMessage = (req, res) => {
-    const data = 
-    'Name of someone I have met: Adam Savage';
-    res.status(200).send(data);
-};
-
-module.exports = {
-    displayMessage,
-};
