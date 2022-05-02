be58ebead7bc2c17fc5130fe2cbd2050e0c4a3f0
Regolith247 <77996630+Regolith247@users.noreply.github.com>
2022-05-02 08:56:44 -0600

    actual revision

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const routes = require('express').Router();
+const express = require('express')
 const router = express.Router();
 
 routes.use('/contacts', require('./contacts'))
